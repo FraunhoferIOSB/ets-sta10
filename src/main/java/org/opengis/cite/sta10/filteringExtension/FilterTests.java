25f1e5044a0a81d404ade169fc5a2ec526b67568
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2016-10-12 09:21:33 +0200

    * Cleanups

--- src/main/java/org/opengis/cite/sta10/filteringExtension/FilterTests.java
+++ src/main/java/org/opengis/cite/sta10/filteringExtension/FilterTests.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2016 Hylke van der Schaaf.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.opengis.cite.sta10.filteringExtension;
 
 import de.fraunhofer.iosb.ilt.sta.ServiceFailureException;
@@ -54,10 +38,7 @@ import org.testng.annotations.Test;
 import org.threeten.extra.Interval;
 
 /**
- * TODO.
- * <ul>
- * <li>geo.length</li>
- * </ul>
+ * Some odd tests.
  *
  * @author Hylke van der Schaaf
  */
--- src/main/java/org/opengis/cite/sta10/util/EntityUtils.java
+++ src/main/java/org/opengis/cite/sta10/util/EntityUtils.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2016 Hylke van der Schaaf.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.opengis.cite.sta10.util;
 
 import de.fraunhofer.iosb.ilt.sta.ServiceFailureException;
@@ -30,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Utility methods for comparing results and cleaning the service.
  *
  * @author Hylke van der Schaaf
  */
@@ -50,10 +35,6 @@ public class EntityUtils {
             this.testOk = testOk;
             this.message = message;
         }
-
-    }
-
-    public void fetchAndCheck() {
 
     }
 
