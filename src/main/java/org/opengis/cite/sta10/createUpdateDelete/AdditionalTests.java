6f84a436ef7a96c263e50da423d5708838bdb4d9
Hylke van der Schaaf <hylke.vds@gmail.com>
2018-05-13 20:01:54 +0200

    Added test to ensure correct FoI generation for Thing with multiple Locations.

---
+++ src/main/java/org/opengis/cite/sta10/createUpdateDelete/AdditionalTests.java
@@ -0,0 +1,125 @@
+package org.opengis.cite.sta10.createUpdateDelete;
+
+import de.fraunhofer.iosb.ilt.sta.ServiceFailureException;
+import de.fraunhofer.iosb.ilt.sta.dao.ObservationDao;
+import de.fraunhofer.iosb.ilt.sta.model.Datastream;
+import de.fraunhofer.iosb.ilt.sta.model.FeatureOfInterest;
+import de.fraunhofer.iosb.ilt.sta.model.Location;
+import de.fraunhofer.iosb.ilt.sta.model.Observation;
+import de.fraunhofer.iosb.ilt.sta.model.ObservedProperty;
+import de.fraunhofer.iosb.ilt.sta.model.Sensor;
+import de.fraunhofer.iosb.ilt.sta.model.Thing;
+import de.fraunhofer.iosb.ilt.sta.model.ext.UnitOfMeasurement;
+import de.fraunhofer.iosb.ilt.sta.service.SensorThingsService;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import org.geojson.Point;
+import org.opengis.cite.sta10.SuiteAttribute;
+import org.opengis.cite.sta10.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Tests additional details not part of the official tests.
+ *
+ * @author Hylke van der Schaaf
+ */
+public class AdditionalTests {
+
+    /**
+     * The logger for this class.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdditionalTests.class);
+    private String rootUri;
+    private SensorThingsService service;
+    private final List<Thing> things = new ArrayList<>();
+    private final List<Datastream> datastreams = new ArrayList<>();
+    private final List<Observation> observations = new ArrayList<>();
+
+    public AdditionalTests() {
+    }
+
+    @BeforeClass()
+    public void setUp(ITestContext testContext) {
+        LOGGER.info("Setting up class.");
+        Object obj = testContext.getSuite().getAttribute(
+                SuiteAttribute.LEVEL.getName());
+        if ((null != obj)) {
+            Integer level = Integer.class.cast(obj);
+            Assert.assertTrue(level.intValue() > 1,
+                    "Conformance level 2 will not be checked since ics = " + level);
+        }
+
+        rootUri = testContext.getSuite().getAttribute(
+                SuiteAttribute.TEST_SUBJECT.getName()).toString();
+        rootUri = rootUri.trim();
+        if (rootUri.lastIndexOf('/') == rootUri.length() - 1) {
+            rootUri = rootUri.substring(0, rootUri.length() - 1);
+        }
+        URL url;
+        try {
+            url = new URL(rootUri);
+            service = new SensorThingsService(url);
+        } catch (MalformedURLException | URISyntaxException ex) {
+            LOGGER.error("Failed to create service uri.", ex);
+        } catch (Exception ex) {
+            LOGGER.error("Unknown Exception.", ex);
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        LOGGER.info("tearing down class.");
+        try {
+            EntityUtils.deleteAll(service);
+        } catch (ServiceFailureException ex) {
+            LOGGER.error("Failed to clean database.", ex);
+        }
+    }
+
+    @Test(description = "Test boolean result values.", groups = "level-2")
+    public void testMultipleLocations() throws ServiceFailureException, URISyntaxException {
+
+        Thing thing = new Thing("Thing 1", "The first thing.");
+
+        Location location1 = new Location("Location 1.0, Address", "The address of Thing 1.", "text/plain", "");
+        thing.getLocations().add(location1);
+        Location location2 = new Location("Location 1.0", "Location of Thing 1.", "application/vnd.geo+json", new Point(8, 51));
+        thing.getLocations().add(location2);
+        Location location3 = new Location("Location 1.0, Directions", "How to find Thing 1 in human language.", "text/plain", "");
+        thing.getLocations().add(location3);
+
+        service.create(thing);
+        things.add(thing);
+
+        Sensor sensor = new Sensor("Sensor 1", "The first sensor.", "text", "Some metadata.");
+        ObservedProperty obsProp = new ObservedProperty("Temperature", new URI("http://ucom.org/temperature"), "The temperature of the thing.");
+        Datastream datastream = new Datastream("Datastream 1", "The temperature of thing 1, sensor 1.", "someType", new UnitOfMeasurement("degree celcius", "°C", "ucum:T"));
+        datastream.setSensor(sensor);
+        datastream.setObservedProperty(obsProp);
+        datastream.setThing(thing);
+
+        service.create(datastream);
+        datastreams.add(datastream);
+
+        ObservationDao doa = service.observations();
+        Observation observation = new Observation(1.0, datastreams.get(0));
+        doa.create(observation);
+        observations.add(observation);
+
+        Observation found;
+        found = doa.find(observation.getId());
+        FeatureOfInterest featureOfInterest = found.getFeatureOfInterest();
+
+    }
+
+}
